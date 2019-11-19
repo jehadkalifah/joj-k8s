7acacf8c277a65c3123749673b70f483f5a9c471
jehadkalifah <jehad.khalifaah@gmail.com>
2019-11-19 18:37:32 +0200

    testing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,6 @@
-sudo kubectl get nodes
+docker push jehaddocker82/test001
+
+
+sudo az login --service-principal -u $AZURE_SP_USERNAME -p $AZURE_SP_PASSWORD --tenant $AZURE_SP_TENANT
+sudo az aks get-credentials --resource-group $AZURE_AKS_RESOURCE_GROUP --name $Azure_AKS_Name
+sudo kubectl apply -f ./scripts
