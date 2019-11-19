71612ce00e5659a6c6340ffe94060dd2bd145e00
jehadkalifah <jehad.khalifaah@gmail.com>
2019-11-19 17:28:58 +0200

    testing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
-sudo az login --service-principal -u $AZURE_SP_USERNAME -p $AZURE_SP_PASSWORD --tenant $AZURE_SP_TENANT
-sudo az aks get-credentials --resource-group $AZURE_AKS_RESOURCE_GROUP --name $Azure_AKS_Name
-sudo kubectl get nodes
+az login --service-principal -u $AZURE_SP_USERNAME -p $AZURE_SP_PASSWORD --tenant $AZURE_SP_TENANT
+az aks get-credentials --resource-group $AZURE_AKS_RESOURCE_GROUP --name $Azure_AKS_Name
+kubectl get nodes
